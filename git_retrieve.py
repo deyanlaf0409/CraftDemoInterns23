c1fd458aaa6dc86fb1a0f9c47993e21b83458a20
deyanlaf0409 <dlafchiev@gmail.com>
2023-04-29 17:21:12 +0300

    initial commit

--- git_retrieve.py
+++ git_retrieve.py
@@ -8,6 +8,7 @@ def get_user_info(github_client, username):
     print(f"Bio: {user.bio}")
 
 
+
 def get_user_repos(github_client, username):
     user = github_client.get_user(username)
     print(f"{username}'s Repositories:")
@@ -15,25 +16,10 @@ def get_user_repos(github_client, username):
         print(f"{repo.name} - {repo.description}")
 
 
-if __name__ == "__main__":
-    personal_access_token = os.environ.get('NODE_AUTH_TOKEN')
-    if not personal_access_token:
-        raise ValueError("GITHUB_TOKEN environment variable not found")
 
-    github_client = Github(personal_access_token)
-    username = "deyanlaf0409"
+personal_access_token = os.environ.get('AUTH_TOKEN')
+github_client = Github(personal_access_token)
+username = "deyanlaf0409"
 
-    get_user_info(github_client, username)
-    get_user_repos(github_client, username)
-
-
-
-
-
-
-
-
-
-
-
-
+get_user_info(github_client, username)
+get_user_repos(github_client, username)
