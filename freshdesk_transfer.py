aa477a1cb69df1e34de5065215d5e0b2382f8970
deyanlaf0409 <dlafchiev@gmail.com>
2023-04-29 21:28:03 +0300

    add email validation

--- freshdesk_transfer.py
+++ freshdesk_transfer.py
@@ -3,7 +3,21 @@ import json
 
 
 def freshdesk_dump(domain, freshdesk_api_key, password, contact_info, headers):
-    r = requests.post("https://" + domain + ".freshdesk.com/api/v2/contacts", auth=(freshdesk_api_key, password),
+    email = contact_info.get("email")
+    if not email:
+        raise ValueError("Missing email in contact_info")
+
+    # Check if contact with same email already exists
+    url = f"https://{domain}.freshdesk.com/api/v2/contacts/autocomplete?term={email}"
+    response = requests.get(url, auth=(freshdesk_api_key, password), headers=headers)
+    if response.status_code == 200:
+        results = json.loads(response.content.decode("utf-8"))
+        if results:
+            print(f"Contact with email {email} already exists, exiting...")
+            return
+
+    # Create new contact if no matching contact found
+    r = requests.post(f"https://{domain}.freshdesk.com/api/v2/contacts", auth=(freshdesk_api_key, password),
                       data=json.dumps(contact_info), headers=headers)
 
     if r.status_code == 201:
