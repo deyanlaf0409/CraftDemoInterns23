c971e39dcc43ac22ba62507e47aa0f8a2e82e863
deyanlaf0409 <dlafchiev@gmail.com>
2023-04-29 22:59:15 +0300

    fix stupid mistake

---
+++ parse_user_input.py
@@ -0,0 +1,9 @@
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Retrieve information of a GitHub User')
+    parser.add_argument('username', type=str, help='GitHub username')
+    parser.add_argument('domain', type=str, help='Freshdesk domain')
+    args = parser.parse_args()
+    return args.username, args.domain
--- start.py
+++ start.py
@@ -1,28 +1,22 @@
-import argparse
 import os
 from github import Github
+from parse_user_input import parse_arguments
 from git_retrieve import get_user_info, get_user_repos
 from freshdesk_transfer import freshdesk_dump
 
 if __name__ == "__main__":
-    # Parse command line arguments
-    parser = argparse.ArgumentParser(description='Retrieve information of a GitHub User')
-    parser.add_argument('username', type=str, help='GitHub username')
-    args = parser.parse_args()
-    username = args.username
+    username, domain = parse_arguments()
 
-
-    github_access_token = os.environ.get('AUTH_TOKEN')
+    github_access_token = os.environ.get('GITHUB_TOKEN')
     github_client = Github(github_access_token)
 
     user = get_user_info(github_client, username)
     get_user_repos(github_client, username)
 
-
     freshdesk_api_key = "m63d39XmdjEJuMNKvYi"
-    domain = "none8662"  # to get from user
     password = "x"
-    contact_info = {"name": user.name, "email": "api_v2_user5@example.com"}
+    contact_info = {"name": user.name, "email": "api_v2_user6@example.com"}
     headers = {"Content-Type": "application/json"}
 
     freshdesk_dump(domain, freshdesk_api_key, password, contact_info, headers)
+
