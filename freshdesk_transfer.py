664aa7797b3a7d2dbf3a289e04b09f7369cfe376
deyanlaf0409 <dlafchiev@gmail.com>
2023-04-30 14:10:58 +0300

    fix

--- freshdesk_transfer.py
+++ freshdesk_transfer.py
@@ -2,15 +2,15 @@ import json
 import requests
 
 
+
 def freshdesk_dump(domain, freshdesk_api_key, password, contact, headers):
     id = contact.get("unique_external_id")
 
-    # Check if contact with same unique_external_id already exists
+    # Check if contact with same email already exists
     url = f"https://{domain}.freshdesk.com/api/v2/contacts/?unique_external_id={id}"
     response = requests.get(url, auth=(freshdesk_api_key, password), headers=headers)
-
     if response.status_code == 200:
-        results = response.json()
+        results = json.loads(response.content.decode("utf-8"))
         if results:
             # Update existing contact
             contact_id = results[0]["id"]
@@ -22,27 +22,23 @@ def freshdesk_dump(domain, freshdesk_api_key, password, contact, headers):
                 print("Failed to update contact, errors are displayed below,")
                 response = json.loads(r.content.decode("utf-8"))
                 print(response["errors"])
+
                 print("x-request-id : " + r.headers['x-request-id'])
                 print("Status Code : " + str(r.status_code))
             return
 
     # Create new contact if no matching contact found
-    elif response.status_code == 404:
-        r = requests.post(f"https://{domain}.freshdesk.com/api/v2/contacts", auth=(freshdesk_api_key, password),
-                          data=json.dumps(contact), headers=headers)
+    r = requests.post(f"https://{domain}.freshdesk.com/api/v2/contacts", auth=(freshdesk_api_key, password),
+                      data=json.dumps(contact), headers=headers)
 
-        if r.status_code == 201:
-            print("Contact created successfully, the response is given below" + r.content.decode("utf-8"))
-            print("Location Header : " + r.headers['Location'])
-        else:
-            print("Failed to create contact, errors are displayed below,")
-            response = json.loads(r.content.decode("utf-8"))
-            print(response["errors"])
-            print("x-request-id : " + r.headers['x-request-id'])
-            print("Status Code : " + str(r.status_code))
+    if r.status_code == 201:
+        print("Contact created successfully, the response is given below" + r.content.decode("utf-8"))
+        print("Location Header : " + r.headers['Location'])
     else:
-        print("Failed to fetch contact, errors are displayed below,")
-        response = json.loads(response.content.decode("utf-8"))
+        print("Failed to create contact, errors are displayed below,")
+        response = json.loads(r.content.decode("utf-8"))
         print(response["errors"])
-        print("x-request-id : " + response.headers['x-request-id'])
-        print("Status Code : " + str(response.status_code))
+
+        print("x-request-id : " + r.headers['x-request-id'])
+        print("Status Code : " + str(r.status_code))
+
