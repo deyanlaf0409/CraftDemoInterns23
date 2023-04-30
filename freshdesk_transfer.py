d1883a16bf83877152531ff629db718fa489cf08
deyanlaf0409 <dlafchiev@gmail.com>
2023-04-30 13:18:59 +0300

    added functionality

--- freshdesk_transfer.py
+++ freshdesk_transfer.py
@@ -7,7 +7,7 @@ def freshdesk_dump(domain, freshdesk_api_key, password, contact, headers):
     id = contact.get("unique_external_id")
 
     # Check if contact with same email already exists
-    url = f"https://{domain}.freshdesk.com/api/v2/contacts/autocomplete?term={id}"
+    url = f"https://{domain}.freshdesk.com/api/v2/contacts/?unique_external_id={id}"
     response = requests.get(url, auth=(freshdesk_api_key, password), headers=headers)
     if response.status_code == 200:
         results = json.loads(response.content.decode("utf-8"))
@@ -17,7 +17,7 @@ def freshdesk_dump(domain, freshdesk_api_key, password, contact, headers):
             url = f"https://{domain}.freshdesk.com/api/v2/contacts/{contact_id}"
             r = requests.put(url, auth=(freshdesk_api_key, password), data=json.dumps(contact), headers=headers)
             if r.status_code == 200:
-                print(f"Contact with {id} updated successfully")
+                print(f"Contact with id: {id} updated successfully")
             else:
                 print("Failed to update contact, errors are displayed below,")
                 response = json.loads(r.content.decode("utf-8"))
