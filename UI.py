44f5d5c6622b2195b05085a76c6b3a7a9bd69063
deyanlaf0409 <dlafchiev@gmail.com>
2023-05-01 12:28:07 +0300

    fix stuff

--- UI.py
+++ UI.py
@@ -7,13 +7,16 @@ if __name__ == "__main__":
     root.title('GitHub User Information')
     root.geometry('400x200')
 
-    username_label = tk.Label(root, text='GitHub Username:')
+    # Set the background color
+    root.configure(background='#282828')
+
+    username_label = tk.Label(root, text='GitHub Username:', bg='#282828', fg='white')
     username_label.pack(pady=10)
 
     username_entry = tk.Entry(root)
     username_entry.pack(pady=5)
 
-    domain_label = tk.Label(root, text='Freshdesk Domain:')
+    domain_label = tk.Label(root, text='Freshdesk Domain:', bg='#282828', fg='white')
     domain_label.pack(pady=10)
 
     domain_entry = tk.Entry(root)
@@ -23,6 +26,4 @@ if __name__ == "__main__":
     start_button.pack(pady=10)
 
     root.mainloop()
-
-
 
